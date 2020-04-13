0da7f31317c96e3b6f8f73d52d990a4efe277caf
rmandalam <39168588+rmandalam@users.noreply.github.com>
2020-04-13 11:47:17 -0400

    Add files via upload

---
+++ CS125_Project.h
@@ -0,0 +1,27 @@
+#ifndef CS_125_PROJECT_H
+#define CS_125_PROJECT_H
+
+//Beginning Function
+void beginning();
+
+//Functions relating to multiplayer game
+void modelH();
+
+//Functions relating to single player game
+void singleH();
+
+//Locations
+void warehouse();
+void nighttime();
+
+//Functions relating to your apartment
+void home();
+void christopher();
+
+//Ending Functions
+void goodEnding();
+void death();
+void caught();
+void decentEnding();
+
+#endif //CS125_PROJECT.H
